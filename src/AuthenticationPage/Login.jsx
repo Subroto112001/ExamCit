bb57adf1a1fdaea26052b3543b07191fcdcfa3fe
Subroto112001 <subrotokumarbarman@gmail.com>
2025-06-21 00:15:30 +0600

    update

--- src/AuthenticationPage/Login.jsx
+++ src/AuthenticationPage/Login.jsx
@@ -1,9 +1,69 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import {
+  getAuth,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  GoogleAuthProvider,
+} from "firebase/auth";
+import { getDatabase, push, ref, set } from "firebase/database";
+import { useNavigate } from 'react-router';
 const Login = () => {
+
+  const auth = getAuth();
+  const db = getDatabase();
+  const navigate = useNavigate();
+
+  const [logemail, setLogEmail] = useState("")
+  const [logpassword, setLogPassword] = useState("")
+
+
+  const takeLoginvalue = (e) => {
+    const { id, value } = e.target;
+    if (id == "email") {
+      setLogEmail(value)
+    }
+    else if (id == "password") {
+      setLogPassword(value)
+    }
+  }
+
+
+  const handleLogin = () => {
+    
+  }
   return (
-    <div>Login</div>
-  )
+    <div className="flex flex-col h-screen justify-center items-center gap-2">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="email">Enter Your Email *</label>
+        <input
+          id="email"
+          type="email"
+          placeholder="Enter Your Email"
+          className="border px-2 py-1 rounded  "
+          onChange={(e) => takeLoginvalue(e)}
+        />
+       
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="password">Enter Your Password *</label>
+        <input
+          id="password"
+          type="password"
+          className="border px-2 py-1 rounded "
+          placeholder="Enter Your Password"
+          onChange={(e) => takeLoginvalue(e)}
+        />
+
+        
+      </div>
+      <button
+        className="px-3 py-1 bg-blue-600 rounded text-white font-medium mt-2 cursor-pointer"
+        onClick={handleLogin}
+      >
+        Login
+      </button>
+    </div>
+  );
 }
 
 export default Login
--- src/AuthenticationPage/Signup.jsx
+++ src/AuthenticationPage/Signup.jsx
@@ -1,16 +1,107 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  sendEmailVerification,
+  updateProfile,
+} from "firebase/auth";
+import { getDatabase, push, ref, set } from "firebase/database";
+import { useNavigate } from "react-router";
 const Signup = () => {
+  const navigate = useNavigate();
+  const auth = getAuth();
+  const db = getDatabase();
+  const [fullname, setFullname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullnameError, setFullnameError] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passworError, setPassworError] = useState("");
+
+  const takevalue = (event) => {
+    console.log(event.target);
+    const { id, value } = event.target;
+    if (id === "Fullname") {
+      setFullname(value);
+    } else if (id === "email") {
+      setemail(value);
+    } else {
+      setPassword(value);
+    }
+  };
+  const handlesignup = async () => {
+    if (!fullname) {
+      setFullnameError("Full Name missing here");
+      return;
+    }
+    if (!email) {
+      setEmailError("Email is missing here");
+      return;
+    }
+    if (!password) {
+      setPassworError("Password is missing here");
+      return;
+    }
+
+    setFullnameError("");
+    setEmailError("");
+    setPassworError("");
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      const user = userCredential.user;
+
+      await updateProfile(user, {
+        displayName: fullname,
+        photoURL: "",
+      });
+
+      const userRef = push(ref(db, "users/"));
+      await set(userRef, {
+        username: fullname,
+        email: email,
+        profile_picture: "",
+        userUid: user.uid,
+      });
+
+      await sendEmailVerification(user)
+        .then(() => {
+          alert(
+            "Verification email has been sent to your inbox."
+          );
+          navigate("/"); 
+        })
+        .catch((error) => {
+          console.error("Error sending verification email:", error);
+          alert("Failed to send verification email: " + error.message);
+        });
+    } catch (error) {
+      console.error("error from firebse:", error.code, error.message);
+    } finally {
+      setFullname("");
+      setemail("");
+      setPassword("");
+    }
+  };
+
   return (
     <div className="flex flex-col h-screen justify-center items-center gap-2">
       <div className="flex flex-col gap-2 ">
-        <label htmlFor="name">Enter Your Full Name *</label>
+        <label htmlFor="Fullname">Enter Your Full Name *</label>
         <input
-          id="name"
+          id="Fullname"
           type="text"
           placeholder="Enter Your Name"
-          className="border px-2 py-1 rounded  "
+          className="border px-2 py-1 rounded"
+          onChange={(e) => takevalue(e)}
         />
+        {fullnameError && (
+          <p className="text-red-500 text-sm">{fullnameError}</p>
+        )}
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="email">Enter Your Email *</label>
@@ -19,14 +110,30 @@ const Signup = () => {
           type="email"
           placeholder="Enter Your Email"
           className="border px-2 py-1 rounded  "
+          onChange={(e) => takevalue(e)}
         />
+        {emailError && <p className="text-red-500 text-sm">{emailError}</p>}
       </div>
-      <div className='flex flex-col gap-2'>
+      <div className="flex flex-col gap-2">
         <label htmlFor="pass">Enter Your Password *</label>
-        <input id="pass" type="password" className='border px-2 py-1 rounded ' placeholder="Enter Your Password" />
+        <input
+          id="password"
+          type="password"
+          className="border px-2 py-1 rounded "
+          placeholder="Enter Your Password"
+          onChange={(e) => takevalue(e)}
+        />
+
+        {passworError && <p className="text-red-500 text-sm">{passworError}</p>}
       </div>
+      <button
+        className="px-3 py-1 bg-blue-600 rounded text-white font-medium mt-2 cursor-pointer"
+        onClick={handlesignup}
+      >
+        Signup
+      </button>
     </div>
   );
-}
+};
 
-export default Signup
+export default Signup;
